329d6588e32d512a61dd86702a5e11a45cf954e0
bcheung0 <brokenxers@gmail.com>
2017-08-09 23:40:07 -0400

    Helloworld

---
+++ Prog1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    
+    
+    cout << "hello world" << endl;
+    /*int candy=6 ;
+    cout<< 1 << " " << candy << endl;
+    for(int day=2;day<31;day++)
+     {
+            candy=candy+5;
+            
+            
+           cout<< day << " " << candy << endl;
+          
+     }   
+         
+     system("pause");
+     return 0;
+     */
+          
+} 
